6576ee090e2325a406be4576015b72df234f16b3
adong <1226494522@qq.com>
2019-06-12 11:40:46 +0800

    码云测试3东

--- Demo3/src/main/java/com/zd/pojo/Student.java
+++ Demo3/src/main/java/com/zd/pojo/Student.java
@@ -2,6 +2,8 @@ package com.zd.pojo;
 
 public class Student {
     public static void main(String[] args) {
-        System.out.println("傻逼？");
+        //System.out.println("傻逼？");
+        //System.out.println("修复傻逼！");
+        System.out.println("d3修复");
     }
 }
